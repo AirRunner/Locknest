2eefd5c1d09d2493c2cfa59142e21836e3312448
timothecht <timothe.chauvet@efrei.net>
2020-05-16 15:24:51 +0200

    Layout update

--- Android/app/src/main/java/com/securious/locknest/HomeActivity.java
+++ Android/app/src/main/java/com/securious/locknest/HomeActivity.java
@@ -13,6 +13,16 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //If this is the first time the user launches the app
+        //it will go in the presentation mode
+        PrefManager prefManager = new PrefManager(getApplicationContext());
+        if(prefManager.isFirstTimeLaunch()){
+            prefManager.setFirstTimeLaunch(false);
+            startActivity(new Intent(HomeActivity.this, WelcomeActivity.class));
+            finish();
+        }
+
+        //Otherwise it will launch the main activity after 2s
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
