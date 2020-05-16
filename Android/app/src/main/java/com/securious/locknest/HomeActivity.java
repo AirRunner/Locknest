74e85649abc6b470834ee5d7cfa2ab4f0f71def5
timothecht <timothe.chauvet@efrei.net>
2020-05-16 12:27:41 +0200

    Splash screen android

---
+++ Android/app/src/main/java/com/securious/locknest/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.securious.locknest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        } , 2000);
+    }
+}
--- Android/app/src/main/java/com/securious/locknest/MainActivity.java
+++ Android/app/src/main/java/com/securious/locknest/MainActivity.java
@@ -4,17 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //If the user is already logged in, go to the stream activity
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            account_loggedin();
+        }
     }
 
     public void account(View view) {
         Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
+    }
+
+    public void account_loggedin() {
+        Intent intent = new Intent(this, Stream.class);
         startActivity(intent);
     }
 }
